e9ff023b34fbb311aa5d9ce1848cec8e38a926f9
Luis Rebollo <luisrebollo30@gmail.com>
2023-04-09 20:02:27 +0200

    php

--- colores/indexcolores.php
+++ colores/indexcolores.php
@@ -1,3 +1,23 @@
+<?php
+session_start();
+
+$colores = "";
+if (isset($_SESSION["colores"])) {
+    $colores = $_SESSION["colores"];
+}
+
+if (isset($_GET["color"])){
+    if (isset($_SESSION["colores"])){
+        $colores=$_SESSION["colores"];
+    }
+    else{
+        $colores=array();
+    }
+    array_push($colores, $_GET["color"]);
+    $_SESSION["colores"]=$colores;
+}
+header("location:indexcolores.php");
+?>
 <!doctype html>
 <html lang="es">
 <head>
---
+++ colores/menos.php
@@ -0,0 +1,5 @@
+<?php
+setcookie("colores",false,time()-1);
+setcookie("color",false,time()-1);
+header("location:indexcolores.php");
+?>
